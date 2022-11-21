fd05c9cfaa491baedcfdd7bf8ac6178cf7ce2a53
Xepmeyc <lesded423@gmail.com>
2022-11-21 15:50:34 +0300

    1st Commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+//Сделать массив {}-ов для Navlink
+const navItems = ['Home', 'About', 'Contact'];
+
+export const Header = () => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <AppBar component="nav">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        MUI
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        {navItems.map((item) => (
+                            //Заменить Button на Navlink
+                            <Button key={item} sx={{ color: '#fff' }}>
+                                {item}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
